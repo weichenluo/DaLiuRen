069c20606b94c75ff6aae788894ac665ae73031d
Weichen Luo <charlieluo130@gmail.com>
2022-07-25 22:55:33 -0400

    Initial Commit

---
+++ KeXueSuanMing-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeXueSuanMing-iOS
+//
+//  Created by Charlie's macmini_M1 on 2022/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeXueSuanMing-iOS/KeXueSuanMing_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeXueSuanMing_iOSApp.swift
+//  KeXueSuanMing-iOS
+//
+//  Created by Charlie's macmini_M1 on 2022/7/25.
+//
+
+import SwiftUI
+
+@main
+struct KeXueSuanMing_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
